3141632c1b800605c18c13daaeb0d17f3901e0f6
japanese-goblinn <cool45akol@gmail.com>
2020-06-14 21:21:50 +0300

    feat: first week

---
+++ yandex-c-plus-plus/first_week.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+int gcd(int a, int b) {
+    while (b) {
+        a = b;
+        std::swap(a, b);
+    }
+    return a;
+}
+
+void PrintBinary(int number) {
+    std::vector<int> v;
+    while (number > 0) {
+        v.emplace_back(number % 2);
+        number /= 2;
+    }
+    for (int i = v.size() - 1; i >= 0; --i) {
+        std::cout << v[i];
+    }
+}
+
+int main() {
+    std::cout << gcd(10, 25) << "\n";
+    PrintBinary(32);
+    std::cout << std::endl;
+    return 0;
+}
