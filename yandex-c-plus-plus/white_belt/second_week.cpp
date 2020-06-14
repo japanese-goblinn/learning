7c9c03a95cf345aaa98fa8a113e313c728773e68
japanese-goblinn <cool45akol@gmail.com>
2020-06-14 21:35:53 +0300

    refactor: tabs to spaces

--- yandex-c-plus-plus/white_belt/second_week.cpp
+++ yandex-c-plus-plus/white_belt/second_week.cpp
@@ -2,8 +2,8 @@
 
 int main(int argc, char const *argv[])
 {
-	int a;
-	std::cin >> a;
-	std::cout << "Hello World!" << a << "\n";
-	return 0;
+    int a;
+    std::cin >> a;
+    std::cout << "Hello World! " << a << "\n";
+    return 0;
 }
