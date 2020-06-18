b62965279356af066497e20978b92240d30e5316
japanese-goblinn <cool45akol@gmail.com>
2020-06-18 18:41:27 +0300

    feat: monthly tasks

--- yandex-c-plus-plus/white_belt/second_week.cpp
+++ yandex-c-plus-plus/white_belt/second_week.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 #define print(x) cout << x << "\n";
 
-void printVector(std::vector<int> v) {
+void printVector(vector<string>& v) {
     for (auto e : v) 
         cout << e << " ";
     cout << "\n";
@@ -141,8 +141,59 @@ void QueueTask() {
     }
 }
 
+void Add(vector<vector<string>>& tasks, string task, int day) {
+    tasks[day - 1].emplace_back(task);
+}
+
+void Dump(const vector<vector<string>>& tasks, int day) {
+    cout << tasks[day - 1].size() << " ";
+    for (auto task : tasks[day - 1])
+        cout << task << " ";
+    cout << "\n";
+}
+
+void Next(const vector<int>& days_in_months, vector<vector<string>>& tasks, int& current_month) {
+    if (++current_month >= days_in_months.size())
+        current_month = 0;
+    int current_amount_of_days = tasks.size();
+    int new_amount_of_days = days_in_months[current_month];
+    if (new_amount_of_days < current_amount_of_days) {
+        auto diff = current_amount_of_days - new_amount_of_days;
+        for (int i = tasks.size() - diff; i < tasks.size(); ++i)
+            tasks[new_amount_of_days - 1].insert(tasks[new_amount_of_days - 1].end(), tasks[i].begin(), tasks[i].end());
+    }
+    tasks.resize(new_amount_of_days);
+}
+
+void MonthlyTasks() {
+    vector<int> days_in_months = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int current_month = 0;
+    vector<vector<string>> current_month_tasks(days_in_months[current_month]);
+
+    string command;
+
+    int day;
+    string task;
+
+    int operations_count;
+    cin >> operations_count;
+    while (operations_count--) {
+        cin >> command;
+        if (command == "NEXT") {
+            Next(days_in_months, current_month_tasks, current_month);
+        } else if (command == "DUMP") {
+            cin >> day;
+            Dump(current_month_tasks, day);
+        } else if (command == "ADD") {
+            cin >> day;
+            cin >> task;
+            Add(current_month_tasks, task, day);
+        }
+    }
+}
+
 int main(int argc, char const *argv[]) {
-    QueueTask();
+    MonthlyTasks();
     return 0;
 }
         
