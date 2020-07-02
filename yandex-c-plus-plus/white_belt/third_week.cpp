02b7d8018a125a38380371437e20fd378309d11b
japanese-goblinn <cool45akol@gmail.com>
2020-07-02 22:27:33 +0300

    refactor: tabs to spaces

--- yandex-c-plus-plus/white_belt/third_week.cpp
+++ yandex-c-plus-plus/white_belt/third_week.cpp
@@ -17,33 +17,33 @@ void PrintVector(const vector<T>& v) {
 }
 
 void AbsCompare() {
-	int n;
-	cin >> n;
-	vector<int> nums(n);
-	for (auto& num : nums)
-		cin >> num;
-	sort(nums.begin(), nums.end(), [](auto lhs, auto rhs) { return abs(lhs) < abs(rhs); });
-	for (const auto& num : nums)
-		cout << num << " ";
-	cout << "\n";
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (auto& num : nums)
+        cin >> num;
+    sort(nums.begin(), nums.end(), [](auto lhs, auto rhs) { return abs(lhs) < abs(rhs); });
+    for (const auto& num : nums)
+        cout << num << " ";
+    cout << "\n";
 }
 
 string ToLower(string str) {
-	transform(str.begin(), str.end(), str.begin(), [](auto ch) { return tolower(ch); });
-	return str;
+    transform(str.begin(), str.end(), str.begin(), [](auto ch) { return tolower(ch); });
+    return str;
 }
 
 void NoCaseSort() {
-	int n;
-	cin >> n;
-	vector<string> strs(n);
-	for (auto& str : strs)
-		cin >> str;
-	sort(strs.begin(), strs.end(), [](auto lhs, auto rhs) { return ToLower(lhs) < ToLower(rhs); });
-	PrintVector(strs);
+    int n;
+    cin >> n;
+    vector<string> strs(n);
+    for (auto& str : strs)
+        cin >> str;
+    sort(strs.begin(), strs.end(), [](auto lhs, auto rhs) { return ToLower(lhs) < ToLower(rhs); });
+    PrintVector(strs);
 }
 
-int main(int argc, char const *argv[]) {	
-	NoCaseSort();
-	return 0;
+int main(int argc, char const *argv[]) {    
+    NoCaseSort();
+    return 0;
 }
