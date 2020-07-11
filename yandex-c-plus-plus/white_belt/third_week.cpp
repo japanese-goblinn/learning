61f07bc83d66bf87afe9fd86779575130b80f4ac
japanese-goblinn <cool45akol@gmail.com>
2020-07-11 13:11:44 +0300

    feat: firstnames and lastnames 1

--- yandex-c-plus-plus/white_belt/third_week.cpp
+++ yandex-c-plus-plus/white_belt/third_week.cpp
@@ -43,7 +43,61 @@ void NoCaseSort() {
     PrintVector(strs);
 }
 
+struct PersonData {
+    string first_name;
+    string last_name;
+};
+
+class Person {
+private:
+    map<int, PersonData> data;
+    void FindData(string& first_name, string& last_name, int year) {
+        for (const auto& kv : data) {
+            if (kv.first > year)
+                break;
+            if (!kv.second.first_name.empty()) 
+                first_name = kv.second.first_name;
+            if (!kv.second.last_name.empty())
+                last_name = kv.second.last_name;
+        }
+    }
+public:
+    void ChangeFirstName(int year, const string& first_name) {
+        data[year].first_name = first_name;
+    }
+    void ChangeLastName(int year, const string& last_name) {
+        data[year].last_name = last_name;
+    }
+    string GetFullName(int year) {
+        string first_name, last_name;
+        this->FindData(first_name, last_name, year);
+        if (first_name.empty() && last_name.empty())
+            return "Incognito";
+        if (first_name.empty())
+            return last_name + " with unknown first name";
+        else if (last_name.empty())
+            return first_name + " with unknown last name";
+        else
+            return first_name + " " + last_name;
+    }
+};
+
 int main(int argc, char const *argv[]) {    
-    NoCaseSort();
+    Person person;  
+  person.ChangeFirstName(1965, "Polina");
+  person.ChangeLastName(1967, "Sergeeva");
+  for (int year : {1900, 1965, 1990}) {
+    cout << person.GetFullName(year) << endl;
+  }
+  
+  person.ChangeFirstName(1970, "Appolinaria");
+  for (int year : {1969, 1970}) {
+    cout << person.GetFullName(year) << endl;
+  }
+  
+  person.ChangeLastName(1968, "Volkova");
+  for (int year : {1969, 1970}) {
+    cout << person.GetFullName(year) << endl;
+  }
     return 0;
 }
